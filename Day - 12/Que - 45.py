c07a423a904bb265e57527ea7a81aa7a9e2becde
SunnyYadav16 <67675772+SunnyYadav16@users.noreply.github.com>
2021-08-18 06:34:39 +0530

    Daily Python Challenges

---
+++ Day - 12/Que - 45.py
@@ -0,0 +1,13 @@
+"""
+Define a class named Indian which has a static method called printNationality.
+"""
+
+
+class Indian:
+    @staticmethod
+    def printNationality():
+        print("I am Indian.")
+
+
+indian = Indian()
+indian.printNationality()
---
+++ Day - 12/Que - 46.py
@@ -0,0 +1,15 @@
+"""
+Define a class named American and its subclass NewYorker.
+"""
+
+
+class American:
+    print("American")
+
+
+class NewYorker:
+    print("New Yorker")
+
+
+american = American()
+newyork = NewYorker()
